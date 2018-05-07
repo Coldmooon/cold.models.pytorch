0c02a9830d03371ba84f65b201a497a0f639db51
coldmooon <liy@bupt.edu.cn>
2018-05-07 11:52:56 +0800

    Added training script

---
+++ train.sh
@@ -0,0 +1,12 @@
+model=$1
+dataset="svhn"
+
+time=$(date +"%Y%m%d_%H_%M")
+description=""
+post=""
+directory="checkpoints/${model}${description}_${dataset}_${post}_${time}_in_progress"
+
+mkdir -p ${directory}/model
+python main_sequence.py -a $model -b 128 --lr 0.01 --wd 0.0005 --epochs 240 --save $directory --dataset $dataset --se-reduce 16 --gpu 1 /home/coldmoon/Datasets/SVHN/LMDB | tee ${directory}/log.txt
+
+mv ${directory} checkpoints/${model}${description}_${dataset}_${post}_${time}
