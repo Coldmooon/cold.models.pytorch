b2dbdcd2fe08b79d33e64dee63e308638282316a
coldmooon <liy@bupt.edu.cn>
2018-05-07 17:51:11 +0800

    Fixed a bug in main_sequence

--- main_sequence.py
+++ main_sequence.py
@@ -412,7 +412,7 @@ def validate(val_loader, model, criterion):
 def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
     torch.save(state, filename)
     if is_best:
-        shutil.copyfile(filename, os.path.dirname(filename) + 'model_best.pth.tar')
+        shutil.copyfile(filename, os.path.dirname(filename) + '/model_best.pth.tar')
 
 
 class AverageMeter(object):
