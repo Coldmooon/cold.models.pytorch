aa4611a8c90ba7c79b23184cf9891ffa27bd2af8
coldmooon <liy@bupt.edu.cn>
2018-05-07 11:54:56 +0800

    Updated train.sh

--- train.sh
+++ train.sh
@@ -1,5 +1,6 @@
 model=$1
 dataset="svhn"
+gpu=$2
 
 time=$(date +"%Y%m%d_%H_%M")
 description=""
@@ -7,6 +8,6 @@ post=""
 directory="checkpoints/${model}${description}_${dataset}_${post}_${time}_in_progress"
 
 mkdir -p ${directory}/model
-python main_sequence.py -a $model -b 128 --lr 0.01 --wd 0.0005 --epochs 240 --save $directory --dataset $dataset --se-reduce 16 --gpu 1 /home/coldmoon/Datasets/SVHN/LMDB | tee ${directory}/log.txt
+python main_sequence.py -a $model -b 128 --lr 0.01 --wd 0.0005 --epochs 240 --save $directory --dataset $dataset --se-reduce 16 --gpu $gpu /home/coldmoon/Datasets/SVHN/LMDB | tee ${directory}/log.txt
 
 mv ${directory} checkpoints/${model}${description}_${dataset}_${post}_${time}
