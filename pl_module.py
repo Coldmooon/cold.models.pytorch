a78ae7c0b26a807f9a152a34214a4f02d93517dd
coldmooon <liy@bupt.edu.cn>
2018-05-09 16:46:19 +0800

    new PL architecture

--- pl_module.py
+++ pl_module.py
@@ -25,3 +25,39 @@ class PLLayer(nn.Module):
         y = self.avg_pool(x).view(b, c)
         y = self.fc(y).view(b, c, 1, 1)
         return xout * y
+
+class PLminus(nn.Module):
+    def __init__(self, channel, reduction=16):
+        super(PLminus, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.bn = nn.BatchNorm2d(channel, affine=False)
+        self.relu = nn.ReLU(inplace=True)
+        self.pdist = nn.PairwiseDistance(p=2,keepdim=True)
+        self.cos = nn.CosineSimilarity(dim=1, eps=1e-6)
+        self.fc = nn.Sequential(
+                nn.Linear(channel, channel // reduction),
+                nn.ReLU(inplace=True),
+                nn.Linear(channel // reduction, channel),
+                nn.Sigmoid()
+                # nn.Softmax(1)
+                # nn.Tanh()
+        )
+
+
+    def forward(self, xin, xout):
+        b, c, _, _ = xin.size()
+        # aggregation
+        # x = torch.cat([xin, xout], 1)
+        # x = xin + xout
+
+        # similarity:
+        # minus
+        # x = self.relu(self.bn(xout)) - self.relu(self.bn(xin))
+
+        # L2-avg
+        x = (self.avg_pool(self.relu(xin)) - self.avg_pool(self.relu(xout))) ** 2
+        y = x.view(b, c)
+        # compute
+        # y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
+        return self.relu(self.bn(xout * y * 2))
