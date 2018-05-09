6a88323f8feb1bd3c18dbd13f0cf35d6f0f2138f
coldmooon <liy@bupt.edu.cn>
2018-05-09 11:15:59 +0800

    group conv as PL

--- STPLMulti.py
+++ STPLMulti.py
@@ -7,7 +7,7 @@ import torch
 from pl_module import PLLayer
 from se_module import SELayer
 
-__all__ = ['STPLMulti', 'STPLFCNMulti', 'STPLFCNv3', 'stpl11', 'stplfcn9', 'stplagg']
+__all__ = ['STPLMulti', 'STPLFCNMulti', 'STPLFCNv3', 'STPLFCNv4','stpl11', 'stplfcn9', 'stplagg', 'stplgroupconv']
 
 
 class STPLMulti(nn.Module):
@@ -642,6 +642,212 @@ class STPLFCNv3(nn.Module):
                                                                self._digit5(x)]
         return length_logits, digits_logits
 
+
+class STPLFCNv4(nn.Module):
+    def __init__(self, r =16):
+        super(STPLFCNv4, self).__init__()
+
+        self.relu = nn.ReLU(inplace=True)
+        self.pl1 = nn.Conv2d(in_channels=48, out_channels=48, kernel_size=1, groups=48, bias=False),
+        self.bn1 = nn.BatchNorm2d(num_features=48),
+        self.pl2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=1, groups=64, bias=False),
+        self.bn2 = nn.BatchNorm2d(num_features=64),
+        self.pl3 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=1, groups=128, bias=False),
+        self.bn3 = nn.BatchNorm2d(num_features=128),
+
+        self.hidden1 = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=48, kernel_size=5, padding=2, bias=False),
+            nn.BatchNorm2d(num_features=48),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
+            # nn.Dropout(0.2)
+        )
+        self.hidden2 = nn.Sequential(
+            nn.Conv2d(in_channels=48, out_channels=64, kernel_size=5, padding=2, bias=False),
+            nn.BatchNorm2d(num_features=64),
+            nn.ReLU(),
+            # nn.MaxPool2d(kernel_size=2, stride=1, padding=1),
+            nn.Dropout(0.5)
+        )
+        self.hidden3 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=5, padding=2, bias=False),
+            nn.BatchNorm2d(num_features=128),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
+            nn.Dropout(0.5)
+        )
+        self.hidden4 = nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=160, kernel_size=5, padding=2, bias=False),
+            nn.BatchNorm2d(num_features=160),
+            nn.ReLU(),
+            # nn.MaxPool2d(kernel_size=2, stride=1, padding=1),
+            nn.Dropout(0.5)
+        )
+        hidden5 = nn.Sequential(
+            nn.Conv2d(in_channels=160, out_channels=192, kernel_size=5, padding=2, bias=False),
+            nn.BatchNorm2d(num_features=192),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
+            nn.Dropout(0.5)
+        )
+        hidden6 = nn.Sequential(
+            nn.Conv2d(in_channels=192, out_channels=192, kernel_size=5, padding=2, bias=False),
+            nn.BatchNorm2d(num_features=192),
+            nn.ReLU(),
+            # nn.MaxPool2d(kernel_size=2, stride=1, padding=1),
+            nn.Dropout(0.5)
+        )
+        hidden7 = nn.Sequential(
+            nn.Conv2d(in_channels=192, out_channels=192, kernel_size=5, padding=2, bias=False),
+            nn.BatchNorm2d(num_features=192),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
+            nn.Dropout(0.5)
+        )
+        hidden8 = nn.Sequential(
+            nn.Conv2d(in_channels=192, out_channels=192, kernel_size=5, padding=2, bias=False),
+            nn.BatchNorm2d(num_features=192),
+            nn.ReLU(),
+            # nn.MaxPool2d(kernel_size=2, stride=1, padding=1),
+            nn.Dropout(0.5)
+        )
+        hidden9 = nn.Sequential(
+            nn.AvgPool2d(3, stride=1)
+        )
+
+        self._features = nn.Sequential(
+            hidden5,
+            hidden6,
+            hidden7,
+            hidden8
+        )
+
+        self._classifier = nn.Sequential(
+            hidden9
+        )
+
+        self._digit_length = nn.Sequential(nn.Linear(192, 7))
+        self._digit1 = nn.Sequential(nn.Linear(192, 11))
+        self._digit2 = nn.Sequential(nn.Linear(192, 11))
+        self._digit3 = nn.Sequential(nn.Linear(192, 11))
+        self._digit4 = nn.Sequential(nn.Linear(192, 11))
+        self._digit5 = nn.Sequential(nn.Linear(192, 11))
+
+        for m in self.modules():
+        #     if isinstance(m, nn.Conv2d):
+        #         n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+        #         m.weight.data.normal_(0, math.sqrt(2. / n))
+            if isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+        # self.st1fc1 = nn.Linear(54 * 54 * 3, 32)
+        # self.st1fc2 = nn.Linear(32, 32)
+        # self.st1fc3 = nn.Linear(32, 6)
+
+        self.st2fc1 = nn.Linear(27 * 27 * 48, 32)
+        self.st2fc2 = nn.Linear(32, 32)
+        self.st2fc3 = nn.Linear(32, 6)
+
+        self.st3fc1 = nn.Linear(27 * 27 * 64, 32)
+        self.st3fc2 = nn.Linear(32, 32)
+        self.st3fc3 = nn.Linear(32, 6)
+
+        self.st4fc1 = nn.Linear(13 * 13 * 128, 32)
+        self.st4fc2 = nn.Linear(32, 32)
+        self.st4fc3 = nn.Linear(32, 6)
+
+        # self.st1fc3.weight.data.zero_()
+        # self.st1fc3.bias.data.copy_(torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float))
+        self.st2fc3.weight.data.zero_()
+        self.st2fc3.bias.data.copy_(torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float))
+        self.st3fc3.weight.data.zero_()
+        self.st3fc3.bias.data.copy_(torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float))
+        self.st4fc3.weight.data.zero_()
+        self.st4fc3.bias.data.copy_(torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float))
+
+    # def st1(self, x):
+    #
+    #     xs = x.view(x.size(0), -1)
+    #     xs = self.relu(self.st1fc1(xs))
+    #     xs = self.relu(self.st1fc2(xs))
+    #     theta = self.st1fc3(xs)
+    #     theta = theta.view(-1, 2, 3)
+    #
+    #     grid = F.affine_grid(theta, x.size())
+    #     x = F.grid_sample(x, grid)
+    #
+    #     return x
+
+    def st2(self, x):
+
+        xs = x.view(x.size(0), -1)
+        xs = self.relu(self.st2fc1(xs))
+        xs = self.relu(self.st2fc2(xs))
+        theta = self.st2fc3(xs)
+        theta = theta.view(-1, 2, 3)
+
+        grid = F.affine_grid(theta, x.size())
+        x = F.grid_sample(x, grid)
+
+        return x
+
+    def st3(self, x):
+
+        xs = x.view(x.size(0), -1)
+        xs = self.relu(self.st3fc1(xs))
+        xs = self.relu(self.st3fc2(xs))
+        theta = self.st3fc3(xs)
+        theta = theta.view(-1, 2, 3)
+
+        grid = F.affine_grid(theta, x.size())
+        x = F.grid_sample(x, grid)
+
+        return x
+
+    def st4(self, x):
+
+        xs = x.view(x.size(0), -1)
+        xs = self.relu(self.st4fc1(xs))
+        xs = self.relu(self.st4fc2(xs))
+        theta = self.st4fc3(xs)
+        theta = theta.view(-1, 2, 3)
+
+        grid = F.affine_grid(theta, x.size())
+        x = F.grid_sample(x, grid)
+
+        return x
+
+    def forward(self, x):
+        # x = self.st1(x)
+        x = self.hidden1(x)
+        x = self.st2(x)
+        x = self.pl1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.hidden2(x)
+        x = self.st3(x)
+        x = self.pl2(x)
+        x = self.pl2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+        x = self.hidden3(x)
+        x = self.st4(x)
+        x = self.pl3(x)
+        x = self.bn3(x)
+        x = self.relu(x)
+        x = self.hidden4(x)
+        x = self._features(x)
+        x = self._classifier(x)
+        x = x.view(x.size(0), -1)
+
+        length_logits, digits_logits = self._digit_length(x), [self._digit1(x),
+                                                               self._digit2(x),
+                                                               self._digit3(x),
+                                                               self._digit4(x),
+                                                               self._digit5(x)]
+        return length_logits, digits_logits
+
 def stpl11(r=16):
     """Constructs a ResNet-101 model.
 
@@ -667,4 +873,13 @@ def stplagg(r=16):
         pretrained (bool): If True, returns a model pre-trained on ImageNet
     """
     model = STPLFCNv3(r=r)
+    return model
+
+def stplgroupconv(r=16):
+    """Constructs a ResNet-101 model.
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = STPLFCNv4(r=r)
     return model
