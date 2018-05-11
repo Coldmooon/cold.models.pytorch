9ed53d7467dd3cd1a25c840ff19558ba4beeb982
coldmooon <liy@bupt.edu.cn>
2018-05-11 17:22:46 +0800

    Added ResPL and STPL

--- pl_module.py
+++ pl_module.py
@@ -1,6 +1,6 @@
 from torch import nn
 import torch
-
+import torch.nn.functional as F
 class PLLayer(nn.Module):
     def __init__(self, channel, reduction=16):
         super(PLLayer, self).__init__()
@@ -94,5 +94,84 @@ class PLasConv(nn.Module):
 
         x = self.conv1x1(x)
         x = self.relu(x)
+
+        return x
+
+
+class ResPL(nn.Module):
+    def __init__(self, channel, reduction=16):
+        super(ResPL, self).__init__()
+        self.conv1x1 = nn.Conv2d(channel, channel, kernel_size=1, groups=channel)
+        # self.bn = nn.BatchNorm2d(channel, affine=False)
+        self.relu = nn.ReLU()
+
+        self.conv1x1.weight.data.fill_(1)
+        self.conv1x1.bias.data.zero_()
+
+    def forward(self, xin, xout=None):
+
+        if (xout is not None):
+            x = xin + xout
+        else:
+            x = xin
+
+        xin = xin / torch.norm(xin)
+        xout = xout / torch.norm(xout)
+
+        # method (1):
+        # x = self.bn(x)
+        # method (2):
+        # x = (x - x.mean(0, True)) / (x.std(0, True) + 1e-5)
+        # method (3):
+        # norm = x.pow(2).sum(dim=1, keepdim=True).sqrt()+ 1e-6
+        # x = torch.div(x,norm)
+
+        xout = self.conv1x1(xout)
+        x = torch.cat((xin, xout), dim=1)
+        # x = self.relu(x)
+
+        return x
+
+class STPL(nn.Module):
+    def __init__(self, shape, channel, reduction=16):
+        super(STPL, self).__init__()
+        self.conv1x1 = nn.Conv2d(channel, channel, kernel_size=1, groups=channel)
+        # self.bn = nn.BatchNorm2d(channel, affine=False)
+        self.relu = nn.ReLU()
+
+        # self.stavg = nn.AdaptiveAvgPool2d(1)
+        self.stfc1 = nn.Linear(channel * shape * shape, 32)
+        self.stfc2 = nn.Linear(32, 32)
+        self.stfc3 = nn.Linear(32, 6)
+        self.pl_lambda = nn.Linear(32, channel)
+        self.pl_s = nn.Linear(6, 1)
+
+
+        self.stfc3.weight.data.zero_()
+        self.stfc3.bias.data.copy_(torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float))
+        self.conv1x1.weight.data.fill_(1)
+        self.conv1x1.bias.data.zero_()
+        self.pl_s.weight.data.zero_()
+        self.pl_s.bias.data.fill_(1)
+
+    def forward(self, x):
+
+
+        xs = x.view(x.size(0), -1)
+        xs = self.relu(self.stfc1(xs))
+        xs = self.relu(self.stfc2(xs))
+        theta = self.stfc3(xs)
+
+        s   = self.pl_s(theta).mean()
+        lam = self.pl_lambda(xs).mean(0, True)
+
+        theta = theta.view(-1, 2, 3)
+        grid = F.affine_grid(theta, x.size())
+        x = F.grid_sample(x, grid)
+
+        x = x * torch.exp(s * lam * -1).view(1, -1, 1, 1)
+
+        x = self.conv1x1(x)
+        # x = self.relu(x)
 
         return x
