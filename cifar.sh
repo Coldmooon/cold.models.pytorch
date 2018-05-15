6f4f020f73b4a800bbe75fa9eb8e149c1f242ce9
coldmooon <liy@bupt.edu.cn>
2018-05-15 13:44:37 +0800

    Updated cifar.sh

--- cifar.sh
+++ cifar.sh
@@ -16,6 +16,6 @@ echo "python main.py -a $model -b 128 --lr 0.1 --wd 0.0005 --epochs 200 --save $
 
 chmod 777 ${directory}/resume.sh
 
-python main.py -a $model -b 128 --lr 0.1 --wd 0.0005 --epochs 200 --save $directory --dataset $dataset --se-reduce $se --gpu $gpu /home/coldmoon/Datasets/SVHN/LMDB${datadir} | tee ${directory}/log.txt
+python main.py -a $model -b 128 --lr 0.1 --wd 0.0005 --epochs 200 --save $directory --dataset $dataset --se-reduce $se --gpu $gpu /home/coldmoon/Datasets/SVHN/LMDB${datadir} | tee ${directory}/log2.txt
 
 mv ${directory} checkpoints/${model}${description}_${dataset}_${post}_${time}
