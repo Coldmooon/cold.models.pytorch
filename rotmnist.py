34bd9d7119ffca9496dc92347cdca808865a1483
coldmooon <liy@bupt.edu.cn>
2018-05-19 15:31:58 +0800

    Added mnist-rot-12k dataset

---
+++ rotmnist.py
@@ -0,0 +1,79 @@
+import torch.utils.data as data
+import numpy as np
+from PIL import Image
+from torchvision import transforms
+import os
+import os.path
+import torch
+
+class RotMNIST(data.Dataset):
+    training_file = 'training.pt'
+    test_file = 'test.pt'
+    classes = ['0 - zero', '1 - one', '2 - two', '3 - three', '4 - four',
+               '5 - five', '6 - six', '7 - seven', '8 - eight', '9 - nine']
+    class_to_idx = {_class: i for i, _class in enumerate(classes)}
+
+    @property
+    def targets(self):
+        if self.train:
+            return self.train_labels
+        else:
+            return self.test_labels
+
+    def __init__(self, root, train=True, transform=None, target_transform=None, download=False):
+        self.root = os.path.expanduser(root)
+        self.transform = transform
+        self.target_transform = target_transform
+        self.train = train  # training set or test set
+
+        if download:
+            self.download()
+
+        if not self._check_exists():
+            raise RuntimeError('Dataset not found.' +
+                               ' You can use download=True to download it')
+
+        if self.train:
+            self.train_data, self.train_labels = torch.load(
+                os.path.join(self.root, self.training_file))
+        else:
+            self.test_data, self.test_labels = torch.load(
+                os.path.join(self.root, self.test_file))
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+        Returns:
+            tuple: (image, target) where target is index of the target class.
+        """
+        if self.train:
+            img, target = self.train_data[index], self.train_labels[index]
+        else:
+            img, target = self.test_data[index], self.test_labels[index]
+
+        # doing this so that it is consistent with all other datasets
+        # to return a PIL Image
+        img = Image.fromarray(img.numpy(), mode='L')
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return img, target
+
+    def __len__(self):
+        if self.train:
+            return len(self.train_data)
+        else:
+            return len(self.test_data)
+
+    def _check_exists(self):
+        return os.path.exists(os.path.join(self.root, self.training_file)) and \
+            os.path.exists(os.path.join(self.root, self.test_file))
+
+    def download(self):
+        print("to be done!")
+        exit(0)
