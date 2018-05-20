66c77d8e5d7c95f75b53fc8f0c949c8ce7934649
coldmooon <liy@bupt.edu.cn>
2018-05-20 18:49:18 +0800

    Fixed a bug in rotmnist dataset

--- rotmnist.py
+++ rotmnist.py
@@ -54,7 +54,7 @@ class RotMNIST(data.Dataset):
 
         # doing this so that it is consistent with all other datasets
         # to return a PIL Image
-        img = Image.fromarray(img.numpy(), mode='L')
+        img = Image.fromarray(img.numpy())
 
         if self.transform is not None:
             img = self.transform(img)
