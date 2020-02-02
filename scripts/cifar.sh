1fbaa1bba99fa666bdbab36d98a5eef9665b2406
Coldmooon <mingyang@tsinghua.edu.cn>
2020-02-02 13:01:34 +0000

    Fixed a bug for echo backslash \

--- scripts/cifar.sh
+++ scripts/cifar.sh
@@ -18,11 +18,11 @@ directory="checkpoints/${model}${depth}${description}_${dataset}_${post}_${time}
 mkdir -p ${directory}
 cp $0 mission.sh main.py models/${model}.py ${directory}
 
-echo "python -u main.py -a $model -d $depth -b 128 --lr 0.1 --wd 0.0005 --epochs 200 --se-reduce $se \
-     --dist-url 'tcp://127.0.0.1:10000' --dist-backend 'nccl' --multiprocessing-distributed --world-size 1 --rank 0 --gpu $gpu \
-     --save $directory --resume $directory/checkpoint.pth.tar \
-     -j 32 --print-freq 2 --dataset $dataset \
-     /home/workspace/yangli/Developer/cold.models.pytorch/data | tee ${directory}/log.txt" \
+echo "python -u main.py -a $model -d $depth -b 128 --lr 0.1 --wd 0.0005 --epochs 200 --se-reduce $se \\
+     --dist-url 'tcp://127.0.0.1:10000' --dist-backend 'nccl' --multiprocessing-distributed --world-size 1 --rank 0 --gpu $gpu \\
+     --save $directory --resume $directory/checkpoint.pth.tar \\
+     -j 32 --print-freq 2 --dataset $dataset \\
+     /home/workspace/yangli/Developer/cold.models.pytorch/data | tee ${directory}/log.txt" \\
      > ${directory}/resume.sh
 
 chmod 777 ${directory}/resume.sh
--- scripts/imagenet.sh
+++ scripts/imagenet.sh
@@ -20,11 +20,11 @@ cp $0 mission.sh main.py models/${model}.py ${directory}
 
 # -u: https://stackoverflow.com/questions/21662783/linux-tee-is-not-working-with-python
 
-echo "python -u main.py -a $model -d $depth -b 256 --lr 0.1 --wd 0.0001 --epochs 100 --se-reduce $se \
-     --dist-url 'tcp://127.0.0.1:10000' --dist-backend 'nccl' --multiprocessing-distributed --world-size 1 --rank 0 --gpu $gpu \
-     --save $directory --resume $directory/checkpoint.pth.tar \
-     -j 32 --print-freq 2 --dataset $dataset \
-     /ssd_data/yangli/ImageNet2012 | tee ${directory}/log.txt" \
+echo "python -u main.py -a $model -d $depth -b 256 --lr 0.1 --wd 0.0001 --epochs 100 --se-reduce $se \\
+     --dist-url 'tcp://127.0.0.1:10000' --dist-backend 'nccl' --multiprocessing-distributed --world-size 1 --rank 0 --gpu $gpu \\
+     --save $directory --resume $directory/checkpoint.pth.tar \\
+     -j 32 --print-freq 2 --dataset $dataset \\
+     /ssd_data/yangli/ImageNet2012 | tee ${directory}/log.txt" \\
      > ${directory}/resume.sh
 
 chmod 777 ${directory}/resume.sh
